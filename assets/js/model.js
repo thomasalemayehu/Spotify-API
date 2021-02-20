abb3f506c8b02589e33afb13781113cc8e00bfb4
Thomas Alemayehu <thomas2alexmech@gmail.com>
2021-02-20 17:09:46 +0300

    BackEnd: Added Browse Functionality

---
+++ assets/js/model.js
@@ -0,0 +1,66 @@
+async function getToken() {
+  const clientId = "69b5143b7be746089690eeac9adb1804";
+  const clientSecret = "b4b8fea09b6a4983ad2cabefd49fe8bf";
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(clientId + ":" + clientSecret),
+    },
+    body: "grant_type=client_credentials",
+  });
+
+  const data = await result.json();
+  return data.access_token;
+}
+
+async function getAllCategories(token) {
+  const result = await fetch(
+    `https://api.spotify.com/v1/browse/categories?country=US&locale=sv_US`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data;
+}
+
+async function getCategoryPlaylist(token, categoryID) {
+  let result = await fetch(
+    `https://api.spotify.com/v1/browse/categories/${categoryID}/playlists`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data;
+}
+
+async function getTracks(token, playlistID) {
+  let result = await fetch(
+    `https://api.spotify.com/v1/playlists/${playlistID}/tracks`,
+    {
+      method: "GET",
+      headers: { Authorization: "Bearer " + token },
+    }
+  );
+
+  const data = await result.json();
+  return data;
+}
+
+async function getTrack(token, trackID) {
+  let result = await fetch(`https://api.spotify.com/v1/tracks/${trackID}`, {
+    method: "GET",
+    headers: { Authorization: "Bearer " + token },
+  });
+
+  const data = await result.json();
+  return data;
+}
+
+export { getToken, getAllCategories, getCategoryPlaylist, getTracks, getTrack };
